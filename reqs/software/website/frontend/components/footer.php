cb69fb28714be3fb814d104e75b7fb9230b17eea
ababdelo <ababdelo.ed42@gmail.com>
2026-09-09 18:50:34 +0100

    feat: add shared footer component

---
+++ reqs/software/website/frontend/components/footer.php
@@ -0,0 +1,9 @@
+</main>
+<?php require_once __DIR__ . '/chatbot.php'; ?>
+<script src="/assets/js/navSideBar.js"></script>
+<script src="/assets/js/inactivityLogout.js"></script>
+<script src="/assets/js/notifier.js"></script>
+<script src="/assets/js/chatbot.js"></script>
+<?php if (isset($extraJs)) echo $extraJs; ?>
+</body>
+</html>
