7b97d1d14c6d418af8e982c2e9c28ebdbe7d4bfb
ababdelo <ababdelo.ed42@gmail.com>
2026-09-09 18:50:01 +0100

    build: add database init script

---
+++ reqs/software/database/init-db.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -e
+
+echo "[IAS42] MySQL init scripts directory mounted successfully."
+echo "[IAS42] Any .sql or .sh file in /docker-entrypoint-initdb.d will run automatically on first database initialization."
